964f8233340d00611c5c79b7ab16ad7b1dbc678f
Sing Mak <sing.mak@fleetship.com>
2021-05-08 17:58:35 +0800

    add starting point

---
+++ starting-point/build/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// rest of the code remains same
+const app = express_1.default();
+const PORT = 8000;
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.get('/users', (req, res) => {
+    const users = ['John', 'Tom', 'Mary'].map((name, index) => ({
+        id: index,
+        name,
+    }));
+    res.json(users);
+});
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
---
+++ starting-point/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+// rest of the code remains same
+const app = express();
+const PORT = 8000;
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.get('/users', (req, res) => {
+  const users = ['John', 'Tom', 'Mary'].map((name, index) => ({
+    id: index,
+    name,
+  }));
+  res.json(users);
+})
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
